569dfc6b8f97b01926896e7fb9ab432cd3abea02
Zhang Rong Zhan <ra45388791@gmail.com>
2022-03-19 00:49:49 +0800

    修改取得資料的處裡方式以及新增資料中的func屬性來判斷如何操作

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 const fs = require('fs');
 const express = require('express');
 const { log } = require('console');
+const { json } = require('express');
 
 const app = express();
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 
 //開啟cors權限
@@ -21,35 +22,49 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     let jsonData = await getJSON();
+    const newArticleData = await parseFormFunc(req);
+    const eventFunc = newArticleData.func;
+    let eventData = newArticleData.data;
 
-    const newData = await parseFormFunc(req);
-    console.log(newData);
+    //完整資料格式
+    console.log(newArticleData);
     //推入資料
-    jsonData.push(newData);
-    //轉成字串型態
-    let str = JSON.stringify(jsonData);
+    jsonData.push(eventData);
+    //寫入json
+    await writeJson(jsonData);
 
-    //寫入
-    fs.writeFile('./data/item.json', str, (err) => {
-        if (err) throw (err);
-        return
-    })
-    // console.log(jsonData);
     res.send(jsonData);
-
 });
 
-// process.env.PORT
-app.listen(process.env.PORT, () => {
+app.delete('/', async (req, res) => {
+    let jsonData = await getJSON();
+    const getData = await parseFormFunc(req);
+    const eventFunc = getData.func;
+    let eventData = getData.data;
+    //取得id值
+    console.log(eventData.id);
+
+    jsonData.forEach((e, index) => {
+        if (e.id === eventData.id) {
+            jsonData.splice(index, 1);
+        }
+    });
+
+    //寫入json
+    await writeJson(jsonData);
+
+    res.send(jsonData);
+});
+
+// !process.env.PORT
+app.listen(port, () => {
     // console.log(port);
-    console.log('監聽中');
+    console.log('監聽中-端口:' + port);
     console.log('測試自動部屬');
-    console.log('測試自動部屬2');
-    console.log('測試自動部屬3');
     // console.log(process.env.PORT);
 })
 
-//get 取得JSON資料
+//get 讀取JSON資料
 function getJSON() {
     return new Promise((resolve, reject) => {
 
@@ -72,12 +87,34 @@ function parseFormFunc(req) {
         parseFormdata(req, (err, data) => {
             if (err) reject(err);
             resolve(data.fields);
+            return
         })
     }).then((promiseRes) => {
-        const newData = promiseRes;
-        return newData;
-        // console.log(newData);
+        // 轉換成json格式
+
+        const func = promiseRes.func;
+        const data = JSON.parse(promiseRes.data);
+        const jsonData = {
+            func: func,
+            data: data,
+        }
+
+        // console.log(jsonData);
+        return jsonData;
     }).catch((err) => {
         throw err;
     })
+}
+
+//寫入json方法
+function writeJson(event) {
+    //轉成字串型態
+    const str = JSON.stringify(event);
+
+    //寫入
+    fs.writeFile('./data/item.json', str, (err) => {
+        if (err) throw (err);
+        return
+    })
+
 }
