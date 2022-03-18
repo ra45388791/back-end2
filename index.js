daa7a12c637a62d7179ca5aaa4c3724afaf2b1bf
Zhang Rong Zhan <ra45388791@gmail.com>
2022-03-19 02:13:40 +0800

    導入eslint

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
+/* eslint-disable no-unused-vars */
 const http = require('http');
 const url = require('url');
 const parseFormdata = require('parse-formdata');
 const cors = require('cors');
 const fs = require('fs');
 const express = require('express');
-const { log } = require('console');
-const { json } = require('express');
+const {log} = require('console');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 
-//開啟cors權限
+// 開啟cors權限
 app.use(cors());
 
 app.get('/', async (req, res) => {
-    let jsonData = await getJSON();
+    const jsonData = await getJSON();
     res.send(jsonData);
 });
 
 app.post('/', async (req, res) => {
-    let jsonData = await getJSON();
+    const jsonData = await getJSON();
     const newArticleData = await parseFormFunc(req);
     const eventFunc = newArticleData.func;
-    let eventData = newArticleData.data;
+    const eventData = newArticleData.data;
 
-    //完整資料格式
+    // 完整資料格式
     console.log(newArticleData);
-    //推入資料
+    // 推入資料
     jsonData.push(eventData);
-    //寫入json
+    // 寫入json
     await writeJson(jsonData);
 
     res.send(jsonData);
 });
 
 app.delete('/', async (req, res) => {
-    let jsonData = await getJSON();
+    const jsonData = await getJSON();
     const getData = await parseFormFunc(req);
     const eventFunc = getData.func;
-    let eventData = getData.data;
-    //取得id值
+    const eventData = getData.data;
+    // 取得id值
     console.log(eventData.id);
 
     jsonData.forEach((e, index) => {
@@ -50,7 +50,7 @@ app.delete('/', async (req, res) => {
         }
     });
 
-    //寫入json
+    // 寫入json
     await writeJson(jsonData);
 
     res.send(jsonData);
@@ -62,33 +62,40 @@ app.listen(port, () => {
     console.log('監聽中-端口:' + port);
     console.log('測試自動部屬');
     // console.log(process.env.PORT);
-})
+});
 
-//get 讀取JSON資料
-function getJSON() {
+
+/**
+ * get 讀取JSON資料
+ * @return {json}
+ */
+function getJSON () {
     return new Promise((resolve, reject) => {
-
         fs.readFile('./data/item.json', function (err, getData) {
             if (err) reject(err);
-            //取得的資料是數字流型態 要轉成字串
-            let json = JSON.parse(getData.toString());
+            // 取得的資料是數字流型態 要轉成字串
+            const json = JSON.parse(getData.toString());
             resolve(json);
             return;
-        })
-
+        });
     }).then((promiseRes) => {
         return promiseRes;
-    })
+    });
 }
 
-//post 取得表單內容方法
-function parseFormFunc(req) {
+/**
+ * post 取得表單內容方法
+ * @param {object} req
+ * @return {object}
+ */
+function parseFormFunc (req) {
+    console.log(typeof req);
     return new Promise((resolve, reject) => {
         parseFormdata(req, (err, data) => {
             if (err) reject(err);
             resolve(data.fields);
-            return
-        })
+            return;
+        });
     }).then((promiseRes) => {
         // 轉換成json格式
 
@@ -97,24 +104,27 @@ function parseFormFunc(req) {
         const jsonData = {
             func: func,
             data: data,
-        }
+        };
 
         // console.log(jsonData);
         return jsonData;
     }).catch((err) => {
         throw err;
-    })
+    });
 }
 
-//寫入json方法
-function writeJson(event) {
-    //轉成字串型態
+/**
+ * post 取得表單內容方法
+ * @param {json} event
+ * @return {undefined}
+ */
+function writeJson (event) {
+    // 轉成字串型態
     const str = JSON.stringify(event);
 
-    //寫入
+    // 寫入
     fs.writeFile('./data/item.json', str, (err) => {
         if (err) throw (err);
-        return
-    })
-
+        return;
+    });
 }
